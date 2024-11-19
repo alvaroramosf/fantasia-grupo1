fabe71bb1924d43c18982c4eaaf7d82f415f0807
rufus <alvaroramosfustero@gmail.com>
2024-11-19 20:14:35 +0100

    Personaje: pablo motos

---
+++ src/pabloMotos/pabloMotos.java
@@ -0,0 +1,10 @@
+package pabloMotos;
+
+public class pabloMotos {
+
+	public static void main(String[] args) {
+        System.out.println(" Pablo Motos, de mirada aguda como el filo de una espada forjada en el corazón de las estrellas, posee un don único: la capacidad de desentrañar los misterios más oscuros del alma humana."
+	+ "Su nombre resuena como un eco que atravesaba las montañas y se colaba entre los valles más lejanos: Pablo Motos, el Vidente de las Palabras, el Guerrero de Atresmedia, el Guardián de la Verdad."
+        		+ "Camina por los eriales del prime time con paso firme, y su nombre se susurraba entre las gentes de la tierra: \"Allí va el Forjador de Diálogos\", decían los sabios, \"el que se enfrenta a los bulos con solo un gesto y una mirada\".");
+    }
+}
