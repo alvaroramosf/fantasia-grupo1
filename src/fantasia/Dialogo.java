16bdad95d30a135e94143bd360bfca84e6921521
rufus <alvaroramosfustero@gmail.com>
2024-11-19 19:47:33 +0100

    Proyecto inicial

---
+++ src/fantasia/Dialogo.java
@@ -0,0 +1,8 @@
+package fantasia;
+
+public class Dialogo {
+	public static void main(String[] args) {
+        System.out.println("Cuando entré en la taberna, en el rincón más oscuro, vi a Legolas el elfo y a Bibli el enano, sentados frente a la barra con dos jarras de cerveza de mantequilla.");
+    }
+
+}
