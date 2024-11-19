8edb992bb71494b4990704f716b6989d3d61e783
sergio <sergiosanzreinaldo@gmail.com>
2024-11-19 20:23:47 +0100

    Frase gimli 1

--- src/fantasia/Dialogo.java
+++ src/fantasia/Dialogo.java
@@ -3,6 +3,9 @@ package fantasia;
 public class Dialogo {
 	public static void main(String[] args) {
         System.out.println("Cuando entré en la taberna, en el rincón más oscuro, vi a Legolas el elfo y a Bibli el enano, sentados frente a la barra con dos jarras de cerveza de mantequilla.");
-    }
+    
+        System.out.println("Que buena esta la cerveza, NECTAR DE LOS DIOSES");
+	
+	}
 
 }
