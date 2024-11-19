d99e186133827efcae64b69ded1a085dafeaffef
rufus <alvaroramosfustero@gmail.com>
2024-11-19 20:33:47 +0100

    segunda frase pablo motos, arreglado nombre gimbli

--- src/fantasia/Dialogo.java
+++ src/fantasia/Dialogo.java
@@ -2,9 +2,11 @@ package fantasia;
 
 public class Dialogo {
 	public static void main(String[] args) {
-        System.out.println("Cuando entré en la taberna, en el rincón más oscuro, vi a Legolas el elfo y a Bibli el enano, sentados frente a la barra con dos jarras de cerveza de mantequilla.");
+        System.out.println("Cuando entré en la taberna, en el rincón más oscuro, vi a Legolas el elfo y a Gimli el enano, sentados frente a la barra con dos jarras de cerveza de mantequilla.");
     
-        System.out.println("Que buena esta la cerveza, NECTAR DE LOS DIOSES");
+        System.out.println("Que buena esta la cerveza, NECTAR DE LOS DIOSES- Exclamó mi buen compañero Gimli desde la distancia");
+        
+        System.out.println("Yo me acerqué, le enseñé la tarjeta del hormiguero a la hermosa tabernera, y exclamé - Ponme una a mi también, soy el gran Pablo Motos");
 	
 	}
 
