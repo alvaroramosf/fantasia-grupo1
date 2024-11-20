39dc7c0d9d11717b15e3b73ea9ab2cdeefbad158
sergio <sergiosanzreinaldo@gmail.com>
2024-11-20 18:39:27 +0100

    tercera frase de gimli

--- src/fantasia/Dialogo.java
+++ src/fantasia/Dialogo.java
@@ -2,27 +2,29 @@ package fantasia;
 
 public class Dialogo {
 	public static void main(String[] args) {
-        System.out.println("Cuando entré en la taberna, en el rincón más oscuro, vi a Légolas el elfo y a Gimli el enano, "
+        System.out.println("Cuando Pablo Motos entró a la taberna, en el rincón más oscuro, vió a Légolas el elfo y a Gimli el enano, "
         		+ "sentados frente a la barra con dos jarras de cerveza de mantequilla.");
     
-        System.out.println("Que buena esta la cerveza, NECTAR DE LOS DIOSES- Exclamó mi buen compañero Gimli desde la distancia");
+        System.out.println("Que buena esta la cerveza, NECTAR DE LOS DIOSES!- Exclamó el enano Gimli desde la distancia");
         
-        System.out.println("Yo me acerqué, le enseñé la tarjeta del hormiguero a la hermosa tabernera, y exclamé - Ponme una a mi también, soy el gran Pablo Motos");
+        System.out.println("Pablo se acercó, le enseñó la tarjeta del hormiguero a la hermosa tabernera, y exclamó - Ponme una a mi también, soy el gran Pablo Motos");
 	
         System.out.println("Tabernera, ponme lo mismo que a mis compañeros pero el doble de cargado, que los enanos no saben beber como los elfos- Dijo el pícaro de Légolas");
         
-        System.out.println("Con nuestra sed saciada, empezamos a charlar sobre la gran aventura que nos esperaba...");
+        System.out.println("Con su sed saciada, empezaron a charlar sobre la gran aventura que les esperaba...");
         
         System.out.println("Joder Pablo te APESTAN los pies!! Me estás arruinado la cerveza, a pesar de tener la estatura de un enano adulto,"
-        		+ "te huelen peor que un eructo de mi viejo después de cenar ancas de rana de la Ciénaga de los Muertos");
+        		+ "te huelen peor que un eructo de mi viejo después de cenar ancas de rana de la Ciénaga de los Muertos - Escupió el enano de Gimli hacia Pablo");
         
-        System.out.println("Ponme otra rubia, rubia - Le grité a la tabernera. La mía tiene un agujero!!!");
+        System.out.println("Ponme otra rubia, rubia - Gritó Pablo a la tabernera. La mía tiene un agujero!!!");
         
         System.out.println("Entonces Légolas, dándose por aludido por aquel desafortunado comentario, sacó una de sus dagas apuntando directo hacia Pablo" );
         
         System.out.println("¿A quién llamas tú rubia, pequeño hobbit pelirrojo?- Exclamó Légolas");
         
         System.out.println("Acto seguido, Légolas lanzó la daga con la mala suerte de impactar en la jarra de Gimli, tras una fantástica finta de Pablo");
+        
+        System.out.println("Maldito espagueti con orejas puntiagudas, Nadie golpea a un enano! - Gritó Gimli enfadado, mientras se subía a la mesa y se abalanza contra Légolas");
 	}
 
 }
---
+++ src/gimli/Gimli.java
@@ -0,0 +1,23 @@
+package gimli;
+
+public class Gimli {
+
+	public static void main(String[] args) {
+		System.out.println("Gimli es un enano valiente y leal del legendario mundo de El Señor de los Anillos, creado por J.R.R. Tolkien. ");
+		System.out.println("Hijo de Glóin, quien participó en la aventura narrada en El Hobbit, Gimli representa el orgullo y la fortaleza de su raza. ");
+		System.out.println("Es miembro de la Comunidad del Anillo y aporta sus habilidades como guerrero con su fiel hacha, enfrentándose a orcos, trolls y otras criaturas en su misión por destruir el Anillo Único. ");
+		System.out.println("Aunque inicialmente desconfía de los elfos, desarrolla una profunda amistad con Legolas, mostrando que incluso antiguos prejuicios pueden superarse. ");
+		System.out.println("Gimli también tiene un gran sentido del humor y es conocido por su tenacidad y lealtad inquebrantable.");
+		System.out.println();
+	
+		System.out.println("Gimli es un enano robusto y de baja estatura, con un cuerpo compacto y musculoso.");
+		System.out.println("Lleva una espesa barba rojiza que simboliza el orgullo de su linaje, trenzada y adornada según la tradición de su pueblo. ");
+		System.out.println("Su rostro está enmarcado por cejas gruesas y un aire decidido. Viste una armadura pesada, práctica para el combate, y porta un casco decorado con motivos enanos.");
+		System.out.println("Siempre lleva consigo su hacha, su arma característica, y un aspecto que refleja tanto su dureza como su conexión con la tradición ancestral de los enanos.");
+		System.out.println();
+		
+		System.out.println("Una de las frases más emblemáticas de Gimli es:");
+		System.out.println("Nadie golpea a un enano.");
+	}
+
+}
