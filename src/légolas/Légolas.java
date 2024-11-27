7737276c28f4f1bd421a815a2b48e3b6025d2ad1
goncho <gonzalosebastian97@gmail.com>
2024-11-27 15:41:13 +0100

    Añado descripción y frase célebre de Légolas

---
+++ src/légolas/Légolas.java
@@ -0,0 +1,14 @@
+package légolas;
+
+public class Légolas {
+
+	public static void main(String[] args) {
+		System.out.println("Legolas es un elfo sindariano de los bosques de Mirkwood, "
+				+ "hijo del rey Thranduil, y uno de los miembros de la Comunidad del Anillo."
+				+ "Es conocido por su aguda vista, gran oído, "
+				+ "habilidades con el arco y su elegancia. "
+				+ "Su carácter noble y su conexión con la naturaleza reflejan las virtudes tradicionales de los elfos.");
+		System.out.println("Frase típica: \"Nunca pensé que moriría luchando junto a un enano.\"");
+	}
+
+}
